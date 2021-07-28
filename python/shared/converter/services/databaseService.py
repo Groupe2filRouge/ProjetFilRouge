f7dabc2e7653214769250486ddbd1323e8965c64
mohamed <ing.med.chen@gmail.com>
2021-07-28 12:21:17 +0200

    create database services

--- python/shared/converter/services/databaseService.py
+++ python/shared/converter/services/databaseService.py
@@ -10,5 +10,21 @@ class DatabaseService():
     def __init__(self):
         print("init DatabaseService")
         pymongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
-        base_de_donnees = pymongoClient["nom_base_de_donnees"]
-        self.table = base_de_donnees["nom_de_table"]
+        base_de_donnees = pymongoClient["projetFR"]
+        self.redacteurs = base_de_donnees["redacteurs"]
+        
+    def get_redacteur_token(self, git):
+            
+        myquery = { "git": git }
+    
+        l = list(self.redacteurs.find(myquery)) # Converts object to list
+
+        return dumps(l[0]["token"]) # Converts to String
+        
+    def get_redacteur_canal(self, git):
+            
+        myquery = { "git": git }
+    
+        l = list(self.redacteurs.find(myquery)) # Converts object to list
+
+        return dumps(l[0]["canal"]) # Converts to String
