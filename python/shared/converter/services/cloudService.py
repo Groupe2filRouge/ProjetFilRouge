986f35e21bc1ea9978499758635a907a111a1761
EnzoPAGANO <ep69devops@gmail.com>
2021-08-25 10:58:26 +0200

    commit des dernieres modifs pour la mise en ligne et le nettoyage des dossiers, certaines fonctions sont commentees pour eviter les manip sur AWS ou dans les fichiers clones a chque test

--- python/shared/converter/services/cloudService.py
+++ python/shared/converter/services/cloudService.py
@@ -14,7 +14,7 @@ class CloudService():
         self.s3_client = boto3.client('s3')
         #Création du bucket à la volée:
         bucket_name="test-site-enzo"
-        self.s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-3'})
+        #self.s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-west-3'}) #commentée pour éviter d'avoir à supprimer le bucket à chaque fois.
         website_configuration = {'IndexDocument': {'Suffix': 'README.html'}}
         self.s3_client.put_bucket_website(Bucket=bucket_name, WebsiteConfiguration=website_configuration)
         bucket_policy = {'Version': '2012-10-17','Statement': [{'Sid': 'AddPerm','Effect': 'Allow','Principal': '*','Action': ['s3:GetObject'],'Resource': "arn:aws:s3:::%s/*" % bucket_name}]}
@@ -55,8 +55,8 @@ class CloudService():
         #    shutil.rmtree(clone_tmp)
         #except OSError as erreur:
         #    print(f'Error: {clone_tmp} : {erreur.strerror}')
-        try:
-            shutil.rmtree(html_tmp)
-        except OSError as erreur:
-            print(f'Error: {html_tmp} : {erreur.strerror}')    #retourne toujours une erreur "text file busy", impossible de nettoyer le contenu html
+        #try:
+        #    shutil.rmtree(html_tmp)
+        #except OSError as erreur:
+        #    print(f'Error: {html_tmp} : {erreur.strerror}')    #retourne toujours une erreur "text file busy", impossible de nettoyer le contenu html
         return "file uploaded"
